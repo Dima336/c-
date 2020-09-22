fce88ccc04b8f2a1d087d19086fac9ac33d758f8
Dima Vdovin <vdovindima741852963@gmail.com>
2020-09-22 20:28:37 +0300

    Добавляю первую лабу

---
+++ lab 1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab1_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+            double c;
+            Console.Write("Введите величину первого сопротивления(Ом)");
+            a = double.Parse(Console.ReadLine());
+            Console.Write("Введите величину второго сопротивления(Ом)");
+            b = double.Parse(Console.ReadLine());
+            Console.Write("Введите величину третьего сопротивления(Ом)");
+            c = double.Parse(Console.ReadLine());
+            double sum;
+            sum = a + b + c;
+            Console.Write(sum + " - Сопротивление цепи (последовательное соединение) ");
+        }
+    }
+}
