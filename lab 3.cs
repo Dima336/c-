f3bd3f7a804db2f052e4e247d4c0d2ec67050247
Dima Vdovin <vdovindima741852963@gmail.com>
2020-09-22 20:36:59 +0300

    Лабораторна робота 3

---
+++ lab 3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, x1, y1, x2, y2;
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+            x1 = int.Parse(Console.ReadLine());
+            x2 = int.Parse(Console.ReadLine());
+            y1 = int.Parse(Console.ReadLine());
+            y2 = int.Parse(Console.ReadLine());
+
+
+            if ((x > x1) && (x < x2) && (y < y1) && (y > y2))
+            {
+                Console.WriteLine(true);
+
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+            Console.ReadKey();
+        }
+    }
+}
