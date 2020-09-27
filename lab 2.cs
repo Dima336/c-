05a7efeec1ca1ad535956e7dd42a7ccc6e9c7065
Dima336 <71723155+Dima336@users.noreply.github.com>
2020-09-27 11:37:53 +0300

    lab 2

---
+++ lab 2.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        Console.Write("Введите ваше nn");
+        double nn;
+        Console.Write("Введите ваше nk");
+        double nk;
+        nn = double.Parse(Console.ReadLine());
+        nk = double.Parse(Console.ReadLine());
+        decimal result = 0;
+        for (int k = (int)(nn); k <= nk; k++)
+        {
+            result += (decimal)((Math.Pow(-1, k) * k * k - 1) / (k * k + 3));
+        }
+    Console.Write(result +"Ответ");
+  }
+}
