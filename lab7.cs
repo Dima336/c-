63eb9db0a7d1122283fbdbe7c13547c451ceefda
Dima336 <71723155+Dima336@users.noreply.github.com>
2020-12-08 18:26:26 +0200

    Update lab7.cs

--- lab7.cs
+++ lab7.cs
@@ -1,42 +1,58 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System;
 using System.Collections.Generic;
 
 namespace lab_7_dimass
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void enterlist(ref List<int> listInt  )
         {
-            Console.WriteLine("Лаб. 6 вар. 4");
-            List<int> listInt = new List<int>();
-            Console.Write("Введите размер листа: ");
             int sizeList = int.Parse(Console.ReadLine());
-            for(int i = 0; i < sizeList; i++)
+            for (int i = 0; i < sizeList; i++)
             {
-                Console.Write($"Введите {i+1}-ий елемент листа: ");
+                Console.Write($"Введите {i + 1}-ий елемент листа: ");
                 listInt.Add(int.Parse(Console.ReadLine()));
             }
-            Console.Write("Введите пероверочную переменную: ");
-            int k = int.Parse(Console.ReadLine());
-            Console.Write("Индекси елементов которие имеют значение переверочной перенной: ");
+        }
+        public static void index (ref List<int> listInt, int k  )
+        {
             bool temp = false;
-            for (int i = 0; i < sizeList; i++)
+            for (int i = 0; i<listInt.Count; i++)
             {
                 if (listInt[i] == k)
                 {
                     Console.Write("{0 } ", i);
                     temp = true;
+                } 
                 }
-            }
-            if(!temp)
+                 if (!temp)
                 Console.Write("--Таких елементов нет--");
-            int[] ArrFromList = new int[sizeList];
-            for (int i = 0; i < sizeList; i++)
-                ArrFromList[i] = listInt[i];
+
+        }
+        public static void printarrayint(in int[]  ArrFromList)
+        {
             Console.WriteLine();
             Console.Write("Масив по исходному листу: ");
-            for (int i = 0; i < sizeList; i++)
+            for (int i = 0; i < ArrFromList.Length; i++)
                 Console.Write($"{ArrFromList[i]} ");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лаб. 6 вар. 4");
+            List<int> listInt = new List<int>();
+            Console.Write("Введите размер листа: ");
+            enterlist(ref listInt);
+         
+            Console.Write("Введите пероверочную переменную: ");
+            int k = int.Parse(Console.ReadLine());
+            Console.Write("Индекси елементов которие имеют значение переверочной перенной: ");
+            index(ref listInt, k);
+            int[] ArrFromList = new int[listInt.Count];
+            for (int i = 0; i < listInt.Count; i++)
+                ArrFromList[i] = listInt[i];
+            printarrayint(in ArrFromList);
             Console.WriteLine();
             Console.ReadLine();
         }
